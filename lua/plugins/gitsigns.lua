6f917810140c544b131894e650138cf9f03e2813
Pablo Luque Salguero <pablols114@gmail.com>
2023-10-01 18:52:00 +0200

    First commit

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      {
+        "<leader>ght",
+        "<cmd>Gitsigns toggle_current_line_blame<cr>",
+        desc = "Toggles blame in the current line",
+      },
+    },
+  },
+}
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,37 @@
+return {
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        ltex = {
+          settings = {
+            ltex = { language = "es" },
+          },
+        },
+      },
+      -- setup = {
+      --   ltex = {
+      --       settings = {
+      --         ltex = { language = "es_ES" },
+      --       },
+      --   },
+      -- },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "pyright",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+      },
+    },
+  },
+}
